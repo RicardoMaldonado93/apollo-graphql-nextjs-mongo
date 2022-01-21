5a9801b8dc46969dc75ee8dd77582920df0a6893
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2022-01-21 01:01:03 -0300

    added config to connect to bd

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV}`) });
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.info("connected to database");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectToDB;
