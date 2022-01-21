f42ec568fa5670433bafa80915530190a1668274
Ricardo Maldonado <maldonadoricardo93@gmail.com>
2022-01-21 02:36:01 -0300

    added integration with DB and created user save

--- db/resolvers/users.js
+++ db/resolvers/users.js
@@ -1,15 +1,31 @@
+const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
 
 const userResolvers = {
-  Query:{
-    getUsers: () => "some user"
+  Query: {
+    getUsers: () => "some user",
   },
-  Mutation:{
-    newUser: (_,{input})=> {
+  Mutation: {
+    newUser: async (_, { input }) => {
       console.log(input);
-      
-      return "user created ..."
-    }
-  }
+      const { email, password } = input;
+
+      //check if the user exist
+      const isUserExistent = !!(await User.findOne({ email }));
+      if (isUserExistent) throw new Error("the user already exist.");
+
+      // create hash for the user password
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      try {
+        // save into DB
+        const newUser = new User(input);
+        newUser.save();
+        return newUser;
+      } catch (error) {}
+    },
+  },
 };
 
 module.exports = userResolvers;
